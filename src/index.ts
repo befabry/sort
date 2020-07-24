d6e71c63805d8f1768e446b27777290ab78f67b7
befabry <fabrybenjamin5@gmail.com>
2020-07-24 17:59:34 +0200

    Abstract class implementation

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
-import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
 console.log(numbersCollection.data);
-sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection("Xaayb");
-const sorter2 = new Sorter(charactersCollection);
 console.log(charactersCollection.data);
-sorter2.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+linkedList.print();
+linkedList.sort();
+linkedList.print();
